e0a908bd13d46e81ab7ffc025580fde156c37c13
Andira Mahendra S <84491959+andirams@users.noreply.github.com>
2023-06-06 20:18:45 +0700

    update mobile test

---
+++ src/test/java/test/automation/stepdefinitions/LoginSteps.java
@@ -0,0 +1,13 @@
+package test.automation.stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Assert;
+import test.automation.pages.LoginScreen;
+
+public class LoginSteps {
+    LoginScreen loginScreen = new LoginScreen();
+}
