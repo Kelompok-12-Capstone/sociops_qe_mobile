8bc990a460cbef420ffe2dc8bca5c7959066deb4
Elisandranur <125653462+Elisandranur@users.noreply.github.com>
2023-06-14 13:51:41 +0700

    fourth commit, add sleep timer

--- src/test/java/test/automation/stepdefinitions/LoginSteps.java
+++ src/test/java/test/automation/stepdefinitions/LoginSteps.java
@@ -14,18 +14,21 @@ public class LoginSteps {
 
 // Verify Email field outline when user feeling without email type or incomplete email //
     @Given("I am open app socioPs")
-    public void openScoiops(){
+    public void openScoiops() throws InterruptedException {
         loginScreen.validateOnLoginScreen();
+        Thread.sleep(5000);
     }
     @When("on the login screen")
-    public void onLoginScreen(){
+    public void onLoginScreen() throws InterruptedException {
         loginScreen.validateOnLoginScreen();
         boolean actual = loginScreen.validateOnLoginScreen();
         Assert.assertTrue(actual);
+        Thread.sleep(5000);
     }
     @And("I input incomplete email")
-    public void inputIncompleteEmail(){
+    public void inputIncompleteEmail() throws InterruptedException {
         loginScreen.inputFieldEmail("nice");
+        Thread.sleep(1000);
     }
     @Then("I get result the message")
     public void getMessage(){
@@ -34,70 +37,79 @@ public class LoginSteps {
 
 // Verify Email field outline when user feeling with email type //
     @And ("I input email")
-    public void inputRightEmail(){
+    public void inputRightEmail() throws InterruptedException {
         loginScreen.inputFieldEmail("absor@gmail.com");
+        Thread.sleep(1000);
     }
 
 // Verify password field outline when user feeling password //
     @And("I input password")
-    public void inputRightPassword(){
+    public void inputRightPassword() throws InterruptedException {
         loginScreen.inputFieldPassword("11223344");
+        Thread.sleep(1000);
     }
 
 // User can type a text, symbol, and number in email field
     @And ("Click login with email field")
     public void clickloginEmail(){}
     @And("Input email field")
-    public void insertEmailField(){
+    public void insertEmailField() throws InterruptedException {
         loginScreen.inputFieldPassword("absor123@gmail.com");
+        Thread.sleep(1000);
     }
 
 // User can type a text, symbol, and number in password field
     @And ("Click login with password field")
     public void clickPASSField(){}
     @And ("Input password field")
-    public void inputPASSField(){
+    public void inputPASSField() throws InterruptedException {
         loginScreen.inputFieldPassword("1122ABCD");
+        Thread.sleep(1000);
     }
 
 // Verify user can log in with account SOCIOPS
     @And ("I click Login Button")
-    public void clickLoginButton(){
+    public void clickLoginButton() throws InterruptedException {
         loginScreen.clickButtonLogin();
+        Thread.sleep(1000);
     }
     @Then ("I am on dashboard page")
     public void onDashboard(){
-
     }
 
 // Verify user can't log in without input password
     @And ("I input blank password")
-    public void inputBlankPass(){
+    public void inputBlankPass() throws InterruptedException {
         loginScreen.inputFieldPassword("");
+        Thread.sleep(1000);
     }
     @Then ("I get message")
     public void getMessageNotify(){}
 
  // Verify user can't log in without input email
     @And ("I input blank email")
-    public void inputBlankEmail(){
+    public void inputBlankEmail() throws InterruptedException {
         loginScreen.inputFieldEmail("");
+        Thread.sleep(1000);
     }
     @And ("I input  password")
     public void inputPassword(){}
 
 //Verify User can't log in with unregister email
     @And ("I input unregister email")
-    public void inputUnregistEmail(){
+    public void inputUnregistEmail() throws InterruptedException {
         loginScreen.inputFieldEmail("elisandra@mail.com");
+        Thread.sleep(1000);
     }
     @And ("I input unregister password")
-    public void inputunregistPass(){
-        loginScreen.inputFieldPassword("HaiElisa12");}
+    public void inputunregistPass() throws InterruptedException {
+        loginScreen.inputFieldPassword("HaiElisa12");
+        Thread.sleep(1000);}
 
 // Verify user can't log in with inccorect password
     @And ("I input incorrect password")
-    public void incorrectPassword(){
+    public void incorrectPassword() throws InterruptedException {
         loginScreen.inputFieldPassword("123?{++<");
+        Thread.sleep(1000);
     }
 }
--- src/test/java/test/automation/stepdefinitions/RegisterSteps.java
+++ src/test/java/test/automation/stepdefinitions/RegisterSteps.java
@@ -17,10 +17,11 @@ public class RegisterSteps {
 
 // Verify register feature
     @Given ("I Open SOCIOPS Apps")
-    public void openApps(){
+    public void openApps() throws InterruptedException {
         loginScreen.validateOnLoginScreen();
         boolean actual = loginScreen.validateOnLoginScreen();
         Assert.assertTrue(actual);
+        Thread.sleep(5000);
     }
     @And ("I am on the login page and scroll down")
     public void loginPageScroll(){
@@ -31,32 +32,38 @@ public class RegisterSteps {
         registerScreen.clickButtonBuatAkun();
     }
     @And ("Input email and password")
-    public void inputEmailPassword(){
+    public void inputEmailPassword() throws InterruptedException {
         registerScreen.inputEmailRegist("elisa@mail.com");
+        Thread.sleep(1000);
         registerScreen.inputPassRegist("12312389");
+        Thread.sleep(1000);
     }
     @And ("Click Register")
-    public void clickRegister(){
+    public void clickRegister() throws InterruptedException {
         registerScreen.clickToRegist();
+        Thread.sleep(1000);
     }
 
 // Verify user can't register with invalid email
     @And ("Input invalid email and password")
-    public void inputInvalidEmailPass(){
+    public void inputInvalidEmailPass() throws InterruptedException {
         registerScreen.inputEmailRegist("absorrr1111");
+        Thread.sleep(1000);
     }
     @Then ("i get messages registration")
     public void getMessRegist(){}
 
 // Verify user cannot create account with email that already exist
     @And ("Input already registered email and password")
-    public void inputRegisteredEmail(){
+    public void inputRegisteredEmail() throws InterruptedException {
         registerScreen.inputEmailRegist("absor@gamil.com");
+        Thread.sleep(1000);
     }
 
 // Verify user cannot create account with invalid password
     @And ("Input email and 6 char password")
-    public void inputInvalidPass(){
+    public void inputInvalidPass() throws InterruptedException {
         registerScreen.inputPassRegist("AAAB26");
+        Thread.sleep(1000);
     }
 }
